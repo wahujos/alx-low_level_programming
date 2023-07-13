d89af3c9ca74d7edd468196c1bcb2d62a3865124
wahujos <jwahu30@gmail.com>
2023-07-13 10:00:27 -0700

    6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Size of a char: ",sizeof(char)," byte(s)");
-	puts("Size of an int: ",sizeof(int)," byte(s)");
-	puts("Size of a long int: ",sizeof(long int)," byte(s)");
-	puts("Size of a long long int: ",sizeof(long long int)," byte(s)");
-	puts("Size of a float: ",sizeof(float)," byte(s)");
+	puts("Size of a char: ", sizeof(char)," byte(s)");
+	puts("Size of an int: ", sizeof(int)," byte(s)");
+	puts("Size of a long int: ", sizeof(long)," byte(s)");
+	puts("Size of a long long int: ", sizeof(long long)," byte(s)");
+	puts("Size of a float: ", sizeof(float)," byte(s)");
 	return (0);
 }
