1ede68ab9c9426cea1ac7d552ad2e966f9d7cab6
wahujos <jwahu30@gmail.com>
2023-07-17 08:37:34 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Loops combinations
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+	for (j = 1; j < 100; j++)
+	{
+	if (i < j)
+	{
+	putchar((i / 10) + 48);
+	putchar((i % 10) + 48);
+	putchar(' ');
+	putchar((i / 10) + 48);
+	putchar((i % 10) + 48);
+	if ((i != 98) || (j != 99))
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
