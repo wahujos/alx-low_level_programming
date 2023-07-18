652963ed8dedd7b04b07d901ae3b3e212cb7e311
wahujos <jwahu30@gmail.com>
2023-07-18 14:59:03 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two numbers.
+ * @num1: First Number
+ * @num2: Second Number
+ * Return: result of sum
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
+
