ce0b6bfb57e621edfae3b0874447ddd30798e2f9
wahujos <jwahu30@gmail.com>
2023-07-18 14:14:03 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+	int mul;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			mul = a * b;
+			_putchar(mul);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
+
