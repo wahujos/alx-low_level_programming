91db435514731be745072ea55a580d31021cdb92
wahujos <jwahu30@gmail.com>
2023-07-18 14:33:08 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		for (b = 0; b <= 9; b++)
 		{
 			mul = a * b;
-			if ((mul / 10 ) == 0)
+			if ((mul / 10) == 0)
 			{
 				_putchar(' ');
 			}
@@ -23,8 +23,11 @@ void times_table(void)
 				_putchar((mul / 10) + '0');
 			}
 			_putchar((mul % 10) + '0');
+			if (b < 9)
+			{
 			_putchar(',');
 			_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
