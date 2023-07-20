f468aa8557780b0f672d09b2808762a6c74825f7
wahujos <jwahu30@gmail.com>
2023-07-20 03:13:55 -0700

    2. 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,13 +20,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else if (c > a && a > b)
 	{
 		largest = c;
 	}
 	else
 	{
-		largest = c;
+		largest = c
 	}
 	return (largest);
 }
