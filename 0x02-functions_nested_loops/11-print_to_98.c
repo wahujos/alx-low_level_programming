f9d1254c71f0c32046dd528979bb61f0f874ba76
wahujos <jwahu30@gmail.com>
2023-07-22 04:58:47 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,33 +7,35 @@
  */
 void print_to_98(int n)
 {
-int c;
-if (n <= 98)
-{
-for (c = n; c <= 98; c++)
-{
-if (c < 98)
-{
-printf("%d, ", c);
-}
-else
-{
-printf("%d", c);
-}
-}
-}
-else
-{
-for (c = n; c > 98; c--)
-{
-if (c > 98)
-{
-printf("%d, ", c);
-}
-else
-{
-printf("%d", c);
-}
-}
-}
+	int c;
+	
+	if (n <= 98)
+	{
+	for (c = n; c <= 98; c++)
+	{
+		if (c < 98)
+		{
+			printf("%d, ", c);
+		}
+		else
+		{
+			printf("%d", c);
+		}
+	}
+	}
+	else
+	{
+	for (c = n; c >= 98; c--)
+	{
+		if (c > 98)
+		{
+			printf("%d, ", c);
+		}
+		else
+		{
+			printf("%d", c);
+		}
+	}
+	}
+	printf("\n");
 }
