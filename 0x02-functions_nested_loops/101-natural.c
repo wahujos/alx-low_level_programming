40003674a414d4a7ac707c311bbd785ddfc5fb28
wahujos <jwahu30@gmail.com>
2023-07-23 15:14:03 -0700

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Gets the sum of multiples
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 1024;
+	int c;
+	int sum = 0;
+
+	for (c = 0; c < num; c++)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
+
+
