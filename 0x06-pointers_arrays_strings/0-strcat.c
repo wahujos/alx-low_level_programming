152262408b10b4e64e3a88c5f3410d9bec8a4d2f
wahujos <jwahu30@gmail.com>
2023-07-27 03:01:43 -0700

    string concatinations

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,18 +8,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int des_len = 0;
-	int i;
+	int i = 0;
+	int j = 0;
 
-	while (dest[des_len] != '\0')
+	while(dest[i] != '\0')
 	{
-		des_len++;
+		i++;
 	}
-	for (i = 0; src[i] != '\0'; ++i)
+	while (src[j] != '\0')
 	{
-		dest[des_len + 1] = src[i];
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	dest[des_len + i] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
+
 
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings.
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be concatinated from src
+ * Return: pointer to resulting string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int src_len = 0;
+	
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	while (src[src_len] < src[n])
+	{
+		dest[dest_len] = src[src_len];
+		dest_len++;
+		src_len++;
+	}
+	dest[dest_len] = '\0';
+	return (dest);
+}
