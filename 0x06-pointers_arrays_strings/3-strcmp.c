adea6cc7ddc7fe7865d28767df8a37f130d503dd
wahujos <jwahu30@gmail.com>
2023-07-27 07:09:09 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - function compates two strings.
+ * @s1: String one for comparison
+ * @s2: String two for comparison
+ * Return: 0 if equal, negative if s1 < s2, positive if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
