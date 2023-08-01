4a07d288f65230e4d8e815ba82b647b0e6b38e19
wahujos <jwahu30@gmail.com>
2023-08-01 02:30:30 -0700

    4. strpbrk

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,6 +7,7 @@
  * Return: number of bytes in the initial segment of s
  *  which consist only of bytes from accept
  */
+/**
 unsigned int _strspn(char *s, char *accept)
 {
 	int len = 0;
@@ -19,3 +20,24 @@ unsigned int _strspn(char *s, char *accept)
 	}
 	return (len);
 }
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+	int len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j=0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+			len = len + 1;
+			continue;
+			}
+		}
+	}
+	return (len);
+}
+
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _strpbrk -  function locates the first occurrence in the strings
+ * @s: string
+ * @accept: string to match
+ * Return:a pointer to the byte in s or NULL otherwise
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				return &s[j];
+			}
+		}
+	}
+	return (NULL);
+}
