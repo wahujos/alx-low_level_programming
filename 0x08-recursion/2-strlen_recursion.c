fa3011a3a836fc8fb15cce43f60d8f8af81f2690
wahujos <jwahu30@gmail.com>
2023-08-02 07:30:14 -0700

    2. Dreams feel real while we're in them

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of string
+ * @s: pointer to the string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
