6598e3cf4e98b07c014ca5163c12f9fa9d05c305
wahujos <jwahu30@gmail.com>
2023-08-02 06:05:57 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a 
+ * string followed by a new line
+ * @s: pointer to the string we need to print
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	
+	}
+}
