26b4e4fc542903f34df552fcc42df6ff5505019a
wahujos <jwahu30@gmail.com>
2023-08-08 14:56:16 -0700

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,6 +19,7 @@ int **alloc_grid(int width, int height)
 	array = malloc(sizeof(int *) * height);
 	if (array == NULL)
 	{
+		free(array);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
@@ -26,6 +27,7 @@ int **alloc_grid(int width, int height)
 		array[i] = malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
+			free(array[i]);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
