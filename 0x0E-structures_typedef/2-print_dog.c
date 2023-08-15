0132354fd26c5588a618105ad230fe9edeaed680
wahujos <jwahu30@gmail.com>
2023-08-15 20:58:29 +0000

    Dog struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - print dog struct
+ * d: pointer to the dog struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if
