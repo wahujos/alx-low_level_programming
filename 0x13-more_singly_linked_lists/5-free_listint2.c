1e7062ea6d75e41d7e25ebd99f9fbbb5856522e9
wahujos <jwahu30@gmail.com>
2023-08-29 05:00:09 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a list
+ * @head: a pointer to the pointer that points to the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	ptr = *head;
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		free(*head);
+		*head = ptr;
+	}
+}
