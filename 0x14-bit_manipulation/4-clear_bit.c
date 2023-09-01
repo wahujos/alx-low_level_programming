ec638015fc713a6a45ce358ca4ba24bf56dffdac
wahujos <jwahu30@gmail.com>
2023-09-01 18:48:36 +0000

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,11 +8,12 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask;
+
 	if (index >= (sizeof(unsigned long int) * 8 - 1))
 	{
 		return (-1);
 	}
 	mask = 1UL << index;
-	*n = *n & ~ (mask);
+	*n = *n & ~(mask);
 	return (1);
 }
