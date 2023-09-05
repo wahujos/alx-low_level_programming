96ca04c1da79330c025ce7c38347e2a8d37f6b34
wahujos <jwahu30@gmail.com>
2023-09-04 18:32:32 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		}
 	if (text_content != NULL)
 	{
-		text_length = strlen(text_content)
+		text_length = strlen(text_content);
 	write_content = write(fd, text_content, text_length);
 	if (write_content == -1)
 	{
