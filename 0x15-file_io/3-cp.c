eafcf3fb3997e157408ff6c6351691543e776316
wahujos <jwahu30@gmail.com>
2023-09-08 00:20:33 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,40 +12,48 @@
  */
 int main(int argc, char *argv[])
 {
-
 	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
-	int fd_to, fd_from;
+	ssize_t bytes_written, bytes_read;
+	char *file_from = argv[1];
+	char *file_to = argv[2];
+	int fd_from, fd_to;
 
 	if (argc != 3)
 	{
-		perror("Usage: cp file_from file_to\n");
-		exit(97);
+		perror("Usage: cp file_from file_to");
+		return (97);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
-	if (fd_to == -1)
-	{
-		perror("Error: Can't write to file");
-		close(fd_to);
-		exit(99);
-	}
-	fd_from = open(argv[1], O_RDONLY);
+	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		perror("Error: Can't write to file");
-		close(fd_from);
-		exit(98);
+		perror("Cant't write to file");
+		return (99);
 	}
-	while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC , S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		perror("Error: cant write to file");
+		close(fd_from);
+		return (99);
+	}
+	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written != bytes_read)
 		{
 			perror("Error: Can't write to file");
-			close(fd_to);
 			close(fd_from);
-			exit(99);
+			close(fd_to);
+			return (99);
 		}
+	}
+	if (bytes_read == -1)
+	{
+		perror("Error: can't read from file");
+		close(fd_to);
+		close(fd_from);
+		return (98);
 	}
 	return (0);
 }
+
