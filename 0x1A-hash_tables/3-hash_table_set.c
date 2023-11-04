09d57fc8163cc34bdeb8a262f96602b1cfd5de93
wahujos <jwahu30@gmail.com>
2023-11-04 00:49:42 +0000

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,13 +10,26 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *current;
 
 	if (key == NULL || ht == NULL || *key == '\0')
 		return (0);
 	index = hash_djb2((const unsigned char *)key) % ht->size;
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
-	if (new_node->key == NULL)
+	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
