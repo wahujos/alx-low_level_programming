3ee08025c467ebe8f8fce666b0a4ec34d4407b98
wahujos <jwahu30@gmail.com>
2024-02-07 11:30:01 +0300

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,5 @@
 /** Prototypes **/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *array, int s);
+void print_array(int *array, int left, int right);
 #endif
